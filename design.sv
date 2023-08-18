69d95aa267a9f3849d79a99d3e115003be40cb00
Jayant <83420282+jayant31j@users.noreply.github.com>
2023-08-18 18:48:39 +1000

    design.sv

--- design.sv
+++ design.sv
@@ -1,38 +1,24 @@
 `timescale 1ns/1ns
-// Half adder
-module halfadder (
-  input a, b,
-  output reg sum, cout
+
+module threebit_counter(
+    input  clk,          // Clock signal
+    input  rst,          // Reset signal
+    input  up_down,      // Up/Down control signal
+    output reg [2:0] count // 3-bit counter output
 );
+    reg [2:0] value = 0;
   
-  always @(*) begin
-    sum = a ^ b;
-    cout = a & b;
-  end
-  
-endmodule
+    always @(posedge clk) begin
+        if (rst) begin
+            value <= 0;
+        end
+        else if (up_down) begin
+            if (value < 7) value <= value + 1;
+        end
+        else begin
+            if (value > 0) value <= value - 1;
+        end
+        count <= value;
+    end
 
-// Full adder
-
-module fulladder (
-  input a, b, ci,
-  output reg s, co
-);
-  always @(*) begin
-    s = a ^ (b ^ ci);
-    co = (a & b) | (ci & (a ^ b));
-  end
-endmodule
-
-// Three-bit comparator
-module threebit (
-  input [2:0] a, b,
-  output A_greater_B,
-  output A_equal_B,
-  output A_smaller_B
-);
-  wire cO, c1;
-  halfadder h1(.a(a[0]), .b(b[0]), .sum(A_smaller_B), .cout(cO));
-  fulladder f1(.a(a[1]), .b(b[1]), .ci(cO), .s(A_equal_B), .co(c1));
-  fulladder f2(.a(a[2]), .b(b[2]), .ci(c1), .s(A_greater_B), .co());
 endmodule
